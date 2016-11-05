f1d42a8c5facc62a6bd531e3e45e6f55b9ba337f
Sebastian Österlund <s.osterlund@vu.nl>
2016-11-06 00:52:27 +0100

    Modified readme and removed VUNET from create_archive.sh

--- create_archive.sh
+++ create_archive.sh
@@ -1,8 +1,2 @@
 #! /bin/bash
-VUNET= #Fill in your vunet ID here
-if [[ -z $VUNET ]]; then
-    echo "Please fill in your VUNET id in create_archive.sh";
-
-fi;
-#zip submission_$VUNET.zip *.java run.sh compile.sh
-#jar -cMf submission.zip ./
+zip submission_$VUNET.zip *.java run.sh compile.sh
